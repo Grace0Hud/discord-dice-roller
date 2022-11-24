faed75187d89c7e85c16dbc22b367bb5b47ec115
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2022-11-23 21:09:17 -0700

    character compiles

--- character.py
+++ character.py
@@ -1,4 +1,32 @@
-class Character:
-    def _init_(self, name):
+class character:
+    name = ""
+    str = 0
+    dex = 0
+    con = 0
+    intel = 0
+    wis = 0
+    cha = 0
+    #constructors, overloaded
+    #takes just the name, then autofills the other values
+    # def _init_(self, name):
+    #     self.name = name
+        # self.str = 0
+        # self.dex = 0
+        # self.con = 0
+        # self.intel = 0
+        # self.cha = 0
+    #takes the name and all stat values and fills them all
+    def __init__(self, name, str=0, dex=0, con=0, intel=0, wis=0, cha=0):
         self.name = name
+        self.str = str
+        self.dex = dex
+        self.con = con
+        self.intel = intel
+        self.wis = wis
+        self.cha = cha
+    
+    #the to string function, 
+    #returns the object in a formatted string
+    def __str__(self):
+         return f"{self.name}\nstr:{self.str}\ndex:{self.dex}\ncon:{self.con}\nintel:{self.intel}\nwis:{self.wis}\ncha:{self.cha}"
     
